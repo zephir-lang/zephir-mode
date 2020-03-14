2cac2b86aef2a4d98ab1b411919491ec77d7f344
Serghei Iakovlev <egrep@protonmail.ch>
2020-03-14 14:29:15 +0200

    Migrate to GitHub Actions

--- test/test-helper.el
+++ test/test-helper.el
@@ -21,7 +21,6 @@
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-
 ;; You should have received a copy of the GNU General Public License
 ;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
