580523083ac763a1b7ce66b9d7f71cef87778ff3
Serghei Iakovlev <egrep@protonmail.ch>
2020-03-19 23:03:23 +0200

    Add initial font locking support

--- zephir-mode.el
+++ zephir-mode.el
@@ -31,27 +31,6 @@
 
 ;;   GNU Emacs major mode for editing Zephir code.
 ;;
-;; Usage: Put this file in your Emacs Lisp path (eg. site-lisp) and add to
-;; your .emacs file:
-;;
-;;   (require 'zephir-mode)
-;;
-;; To use abbrev-mode, add lines like this:
-;;
-;;   (add-hook 'bnf-mode-hook
-;;             '(lambda () (define-abbrev
-;;                           zephir-mode-abbrev-table
-;;                           "ex" "extends")))
-;;
-;; Many options available under the ‘Help->Customize’ submenu.
-;; Options specific to zephir-mode are in
-;;  Programming/Languages/Zephir
-;;
-;; Bugs: Bug tracking is currently handled using the GitHub issue tracker at
-;; `https://github.com/zephir-lang/zephir-mode/issues'
-;;
-;; History: History is tracked in the Git repository rather than in this file.
-;; See URL `https://github.com/zephir-lang/zephir-mode/blob/master/NEWS'.
 
 ;;; Code:
 
@@ -85,11 +64,8 @@
   :type  'hook
   :group 'zephir)
 
-(defcustom zephir-indent-level 4
-  "Amount by which Zephir subexpressions are indented."
-  :type 'integer
-  :group 'zephir
-  :safe #'integerp)
+(defvar bnf-mode-abbrev-table nil
+  "Abbreviation table used in `zephir-mode' buffers.")
 
 
 ;;; Version information
@@ -153,6 +129,35 @@ If point is not inside a comment, return nil.  Uses CTX as a syntax context."
 
 
 ;;; Specialized rx
+
+(eval-when-compile
+  (defconst zephir-rx-constituents
+    `(
+      ;; Builtin declarations.
+      (builtin-decl . ,(rx symbol-start
+                           (or "class"
+                               "interface"
+                               "namespace"
+                               "abstract"
+                               "final")
+                           symbol-end)))
+    "Additional special sexps for `zephir-rx'.")
+
+  (defmacro zephir-rx (&rest sexps)
+    "Zephir-specific replacement for `rx'.
+
+In addition to the standard forms of `rx', the following forms
+are available:
+
+`builtin-dcl'
+     Any valid builtin declaraion.
+
+See `rx' documentation for more information about REGEXPS param."
+    (let ((rx-constituents (append zephir-rx-constituents rx-constituents)))
+      (rx-to-string (cond ((null sexps) (error "No regexp is provided"))
+                          ((cdr sexps)  `(and ,@sexps))
+                          (t            (car sexps)))
+                    t))))
 
 
 ;;; Navigation
@@ -229,6 +234,13 @@ This uses CTX as a current parse state."
 
 ;;; Font Locking
 
+(defvar zephir-font-lock-keywords
+  `(
+    ;; Builtin declaration.
+    (,(zephir-rx (group builtin-decl))
+     1 font-lock-keyword-face))
+  "Font lock keywords for Zephir Mode.")
+
 
 ;;; Alignment
 
@@ -270,24 +282,26 @@ the comment syntax tokens handle both line style \"//\" and block style
 ;;;###autoload
 (define-derived-mode zephir-mode prog-mode "Zephir" ()
   "A major mode for editing Zephir code."
-  :group 'zephir-mode
+  :abbrev-table zephir-mode-abbrev-table
+  :group 'zephir
 
   ;; Comments setup
   (setq-local comment-use-syntax t)
   (setq-local comment-auto-fill-only-comments t)
   (setq-local comment-multi-line t)
   (setq-local comment-start "// ")
-  (setq-local comment-start-skip
-              (eval-when-compile
-                (rx (group (or (: "/" (+ "/"))
-                               (: "/*")))
-                    (* (syntax whitespace)))))
+  (setq-local comment-start-skip "\\(/[*/]+\\)\\s-*")
   (setq-local comment-end "")
 
-  ;; TODO(serghei): Navigation
-  (setq-local indent-line-function #'zephir-indent-line)
-  ;; TODO(serghei): Font locking
-  )
+  ;; Font locking
+  (setq font-lock-defaults
+        '((zephir-font-lock-keywords) ; keywords
+          nil                         ; keywords-only
+          nil                         ; case-fold
+          ))
+
+  ;; Navigation
+  (setq-local indent-line-function #'zephir-indent-line))
 
 
 ;; Invoke zephir-mode when appropriate
